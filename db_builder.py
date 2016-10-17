6dbf3dfb613effd955152c79c9460f09f1219fb0
CH33ZED <zlin3@stuy.edu>
2016-10-17 07:48:43 -0400

    add

---
+++ db_builder.py
@@ -0,0 +1,33 @@
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitates CSV I/O
+
+
+f="discobandit.db"
+
+db = sqlite3.connect(f) #open if f exists, otherwise create
+c = db.cursor()    #facilitate db ops
+
+#==========================================================
+#INSERT YOUR POPULATE CODE IN THIS ZONE
+#...perhaps by beginning with these examples...
+
+q = "CREATE TABLE students (name TEXT, age INTEGER, id INTEGER)"
+with open('peeps.csv') as csvfile:
+     reader = csv.DictReader(csvfile)
+     for row in reader:
+             "INSERT INTO students(row['name'], row['age'],row['id'])"
+             SELECT * FROM students
+c.execute(q)    #run SQL query
+
+
+"q = "CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER)""
+
+
+c.execute(q)
+
+
+#==========================================================
+db.commit() #save changes
+db.close()  #close database
+
+
